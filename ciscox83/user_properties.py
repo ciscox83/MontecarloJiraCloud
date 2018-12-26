f4aa27a6c784b4690ab7278bbc1cae5f1332d7d5
Christian Sisti <christian.sisti@ocado.com>
2018-12-26 11:16:50 +0100

    Separate user and global properties

--- ciscox83/user_properties.py
+++ ciscox83/user_properties.py
@@ -1,7 +1,3 @@
-# Global properties
-jira_api_version = '2'
-
-# Per user properties
 my_jira_account = '<!-- YOUR JIRA CLOUD ACCOUNT HERE -->'
 my_jira_project = '<!-- YOUR PROJECT TAG HERE -->'
 my_jira_auth_token = '<!-- YOUR AUTH TOKEN HERE -->'
