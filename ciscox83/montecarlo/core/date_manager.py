b18803244839ef19025156a37af467387ffbdd19
Christian Sisti <christian.sisti@ocado.com>
2018-12-27 15:55:43 +0100

    Tidy up

--- ciscox83/montecarlo/core/date_manager.py
+++ ciscox83/montecarlo/core/date_manager.py
@@ -6,19 +6,19 @@ class DateManager:
     def __init__(self):
         pass
 
-    # we assume for simplicity that iterations are 1 every week
     @staticmethod
-    def iteration_start_date(iteration_end_date):
-        iteration_end_date = iteration_end_date + " 23:59:59"
-        iteration_end_date = datetime.strptime(iteration_end_date, "%Y/%m/%d %H:%M:%S")
-        a_week_ago = iteration_end_date - timedelta(days=7)
-        a_week_ago = a_week_ago.strftime("%Y/%m/%d %H:%M:%S")[:-9]
-        return a_week_ago + " 00:00"
-
-    @staticmethod
-    def iteration_end_date(date):
+    def adjust_iteration_end_date(date):
         try:
             datetime.strptime(date, '%Y/%m/%d')
         except ValueError:
             raise ValueError("Incorrect data format, should be yyy/mm/dd")
         return date + " 23:59:59"
+
+    # we assume for simplicity that iterations are 1 every week
+    @staticmethod
+    def get_iteration_start_date(end_date):
+        iteration_end_date = datetime.strptime(DateManager.adjust_iteration_end_date(end_date), "%Y/%m/%d %H:%M:%S")
+        a_week_ago = iteration_end_date - timedelta(days=7)
+        a_week_ago = a_week_ago.strftime("%Y/%m/%d %H:%M:%S")[:-9]
+        return a_week_ago + " 00:00"
+
--- ciscox83/tests/montecarlo_unit.py
+++ ciscox83/tests/montecarlo_unit.py
@@ -6,14 +6,14 @@ class MontecarloUnitTest(unittest.TestCase):
     date_manager = DateManager()
 
     def test_that_end_of_iteration_boundary_include_whole_day(self):
-        self.assertEqual("2018/12/27 23:59:59", self.date_manager.iteration_end_date("2018/12/27"))
+        self.assertEqual("2018/12/27 23:59:59", self.date_manager.adjust_iteration_end_date("2018/12/27"))
 
     def test_that_raise_an_error_if_input_format_not_match(self):
-        self.assertRaises(ValueError, self.date_manager.iteration_end_date, "2018-12-27")
+        self.assertRaises(ValueError, self.date_manager.adjust_iteration_end_date, "2018-12-27")
 
     def test_can_get_a_week_ago_date_in_jira_format(self):
         today = "2018/12/27"
-        a_week_ago = self.date_manager.iteration_start_date(today)
+        a_week_ago = self.date_manager.get_iteration_start_date(today)
         self.assertEqual("2018/12/20 00:00", a_week_ago)
 
 
