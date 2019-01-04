066bb67a356085f6412eaf027ac7525c1a3f80e9
Christian Sisti <christian.sisti@ocado.com>
2019-01-04 15:54:49 +0100

    Add a bit of OO

--- ciscox83/montecarlo/core/montecarlo.py
+++ ciscox83/montecarlo/core/montecarlo.py
@@ -3,9 +3,14 @@ from decimal import *
 class Montecarlo:
     def __init__(self, iterations):
         self.iterations = iterations
+        self.real_cycle_times = self.__compute_real_cycle_times()
         pass
 
+
     def get_real_cycle_times(self):
+        return self.real_cycle_times
+
+    def __compute_real_cycle_times(self):
         cycle_times = []
         for iteration in self.iterations:
             cycle_time = 5.0 / iteration.get_completed()
