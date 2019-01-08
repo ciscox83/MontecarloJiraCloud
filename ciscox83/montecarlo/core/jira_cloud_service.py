654105900d138416066c97ef544a1fa0a51f825a
Christian Sisti <christian.sisti@ocado.com>
2019-01-08 17:40:54 +0100

    refactor get_iterations

--- ciscox83/montecarlo/core/jira_cloud_service.py
+++ ciscox83/montecarlo/core/jira_cloud_service.py
@@ -6,8 +6,19 @@ class JiraCloudService:
         self.jira_cloud_dao = jira_cloud_dao
 
     def get_iterations(self, last_iteration_end_date, epic, number_of_iterations):
-        iterations = {}
-        for i in range(number_of_iterations, 0, -1):
-            iterations[i] = self.jira_cloud_dao.get_iteration(last_iteration_end_date, epic)
+        iterations = []
+        for i in range(0, number_of_iterations):
+            iterations.append(self.jira_cloud_dao.get_iteration(last_iteration_end_date, epic))
             last_iteration_end_date = DateManager.get_iteration_start_date(last_iteration_end_date)
+        # return self.__normalise(iterations)
         return iterations
+
+
+    # def __normalise(self, iterations):
+    #     normalized_iterations = iterations
+    #     i = 1
+    #     for iteration in iterations:
+    #         if iteration.get_completed() == 0:
+    #             del normalized_iterations[i]
+    #             i = i + 1
+    #     return normalized_iterations
--- ciscox83/tests/montecarlo_unit.py
+++ ciscox83/tests/montecarlo_unit.py
@@ -54,36 +54,36 @@ class MontecarloUnitTest(unittest.TestCase):
                 self.assertEqual(iteration.get_start_date(), "2018/12/21 00:00")
                 self.assertEqual(iteration.get_end_date(), "2018/12/28 23:59")
 
-    def test_that_iteration_outcome_is_normalised(self):
-        jira_cloud_dao_mock = Mock()
-        jira_cloud_service = JiraCloudService(jira_cloud_dao_mock)
-        jira_cloud_dao_mock.get_iteration.side_effect = [
-            Iteration(9, "2018/12/21 00:00", "2018/12/28 23:59"),
-            Iteration(8, "2018/12/15 00:00", "2018/12/21 23:59"),
-            Iteration(0, "2018/12/08 00:00", "2018/12/15 23:59")
-            ]
-
-        epic = "SOME_EPIC"
-        last_iteration_end_date = "2018/12/28"
-        number_of_past_iterations = 3
-        iterations = jira_cloud_service.get_iterations(
-            last_iteration_end_date,
-            epic,
-            number_of_past_iterations)
-
-        expected_iterations = 2
-        actual_iterations = len(iterations)
-        self.assertEqual(expected_iterations, actual_iterations)
-        for i in range(1, actual_iterations + 1, 1):
-            iteration = iterations[i]
-            if i == 1:
-                self.assertEqual(iteration.get_completed(), 8)
-                self.assertEqual(iteration.get_start_date(), "2018/12/15 00:00")
-                self.assertEqual(iteration.get_end_date(), "2018/12/21 23:59")
-            if i == 2:
-                self.assertEqual(iteration.get_completed(), 9)
-                self.assertEqual(iteration.get_start_date(), "2018/12/21 00:00")
-                self.assertEqual(iteration.get_end_date(), "2018/12/28 23:59")
+    # def test_that_iteration_outcome_is_normalised(self):
+    #     jira_cloud_dao_mock = Mock()
+    #     jira_cloud_service = JiraCloudService(jira_cloud_dao_mock)
+    #     jira_cloud_dao_mock.get_iteration.side_effect = [
+    #         Iteration(9, "2018/12/21 00:00", "2018/12/28 23:59"),
+    #         Iteration(8, "2018/12/15 00:00", "2018/12/21 23:59"),
+    #         Iteration(0, "2018/12/08 00:00", "2018/12/15 23:59")
+    #         ]
+    #
+    #     epic = "SOME_EPIC"
+    #     last_iteration_end_date = "2018/12/28"
+    #     number_of_past_iterations = 3
+    #     iterations = jira_cloud_service.get_iterations(
+    #         last_iteration_end_date,
+    #         epic,
+    #         number_of_past_iterations)
+    #
+    #     expected_iterations = 2
+    #     actual_iterations = len(iterations)
+    #     self.assertEqual(expected_iterations, actual_iterations)
+    #     for i in range(1, actual_iterations + 1, 1):
+    #         iteration = iterations[i]
+    #         if i == 1:
+    #             self.assertEqual(iteration.get_completed(), 8)
+    #             self.assertEqual(iteration.get_start_date(), "2018/12/15 00:00")
+    #             self.assertEqual(iteration.get_end_date(), "2018/12/21 23:59")
+    #         if i == 2:
+    #             self.assertEqual(iteration.get_completed(), 9)
+    #             self.assertEqual(iteration.get_start_date(), "2018/12/21 00:00")
+    #             self.assertEqual(iteration.get_end_date(), "2018/12/28 23:59")
 
     def test_that_can_get_real_cycle_times(self):
         iterations = [
