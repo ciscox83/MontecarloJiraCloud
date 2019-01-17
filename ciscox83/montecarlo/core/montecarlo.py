37f01942534405bca0cfcaa95c1fc88e4ed79b0a
Christian Sisti <christian.sisti@ocado.com>
2019-01-17 12:50:49 +0100

    Compute average iterations needed to complete the work left

--- ciscox83/montecarlo/core/montecarlo.py
+++ ciscox83/montecarlo/core/montecarlo.py
@@ -2,6 +2,7 @@ from decimal import *
 from random import randrange
 
 from ciscox83.global_properties import MONTECARLO_ITERATIONS
+from ciscox83.global_properties import ITERATION_DURATION
 
 
 class Montecarlo:
@@ -21,8 +22,8 @@ class Montecarlo:
             cycle_times.append(Decimal(cycle_time).__round__(2))
         return cycle_times
 
-    def run(self):
-        cycle_time_averages = []
+    def run(self, pending_items):
+        average_iterations = []
         num_of_real_cycle_times = len(self.real_cycle_times)
         for x in range(0, MONTECARLO_ITERATIONS, 1):
             total_days = 0
@@ -30,5 +31,6 @@ class Montecarlo:
                 i = randrange(num_of_real_cycle_times)
                 total_days = total_days + self.real_cycle_times[i]
             average_cycle_time = total_days / num_of_real_cycle_times
-            cycle_time_averages.append(average_cycle_time)
-        return cycle_time_averages
+            average_iteration = round(pending_items * average_cycle_time / ITERATION_DURATION)
+            average_iterations.append(average_iteration)
+        return average_iterations
--- ciscox83/tests/montecarlo_unit.py
+++ ciscox83/tests/montecarlo_unit.py
@@ -22,20 +22,24 @@ class MontecarloUnitTest(unittest.TestCase):
                           Decimal(0.62).__round__(2),
                           Decimal(0.56).__round__(2)])
 
-    def test_that_montecarlo_runs(self):
-        iterations = [
-            Iteration(7, "2018/12/08 00:00", "2018/12/15 23:59"),
-            Iteration(8, "2018/12/15 00:00", "2018/12/21 23:59"),
-            Iteration(9, "2018/12/21 00:00", "2018/12/28 23:59")
+    def test_that_can_compute_average_iterations_to_complete_pending_work_items(self):
+        items_completed_in_iteration = 7
+        pending_items = 21
+        past_iterations = [
+            Iteration(items_completed_in_iteration, "2018/12/08 00:00", "2018/12/15 23:59"),
+            Iteration(items_completed_in_iteration, "2018/12/15 00:00", "2018/12/21 23:59"),
+            Iteration(items_completed_in_iteration, "2018/12/21 00:00", "2018/12/28 23:59")
         ]
 
-        montecarlo = Montecarlo(iterations)
-        cycle_times_averages = montecarlo.run()
-        num_of_averages_cycle_times = len(cycle_times_averages)
-        self.assertEqual(num_of_averages_cycle_times, MONTECARLO_ITERATIONS)
-        for i in range(0, num_of_averages_cycle_times, 1):
-            self.assertGreaterEqual(cycle_times_averages[i], Decimal(0.56).__round__(2))
-            self.assertLessEqual(cycle_times_averages[i], Decimal(0.71).__round__(2))
+        montecarlo = Montecarlo(past_iterations)
+        average_iterations = montecarlo.run(pending_items)
+        num_of_average_iterations = len(average_iterations)
+        self.assertEqual(num_of_average_iterations, MONTECARLO_ITERATIONS)
+        for i in range(0, num_of_average_iterations, 1):
+            self.assertEqual(average_iterations[i], pending_items / items_completed_in_iteration)
+
+
+
 
 
 if __name__ == '__main__':
