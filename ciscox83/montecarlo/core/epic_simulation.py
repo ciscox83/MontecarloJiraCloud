23e205ecdf00a45114240cac44ee42bc5222c578
Christian Sisti <christian.sisti@ocado.com>
2019-04-10 10:15:31 +0200

    Refactoring

---
+++ ciscox83/montecarlo/core/epic_simulation.py
@@ -0,0 +1,36 @@
+from decimal import *
+from random import randrange
+
+from ciscox83.global_properties import MONTECARLO_ITERATIONS
+from ciscox83.global_properties import WORKING_DAYS_IN_ITERATION
+
+
+class EpicSimulator:
+    def __init__(self, iterations):
+        self.iterations = iterations
+        self.past_cycle_times = self.__compute_past_cycle_times()
+        pass
+
+    def __compute_past_cycle_times(self):
+        cycle_times = []
+        for iteration in self.iterations:
+            cycle_time = WORKING_DAYS_IN_ITERATION / iteration.get_completed()
+            cycle_times.append(Decimal(cycle_time).__round__(2))
+        return cycle_times
+
+    #
+    # Uses Montecarlo simulation technique to generate a set of (possible)
+    # number of iterations needed to complete the Epic
+    #
+    def simulate(self, pending_items):
+        simulated_iterations_length = []
+        number_of_past_cycle_times = len(self.past_cycle_times)
+        for x in range(0, MONTECARLO_ITERATIONS, 1):
+            total_days = 0
+            for y in range(0, number_of_past_cycle_times, 1):
+                i = randrange(number_of_past_cycle_times)
+                total_days = total_days + self.past_cycle_times[i]
+            average_cycle_time = total_days / number_of_past_cycle_times
+            iteration_length = round(pending_items * average_cycle_time / WORKING_DAYS_IN_ITERATION)
+            simulated_iterations_length.append(iteration_length)
+        return simulated_iterations_length
--- ciscox83/tests/unit/epic_simulation_test.py
+++ ciscox83/tests/unit/epic_simulation_test.py
@@ -3,26 +3,26 @@ from decimal import *
 
 from ciscox83.global_properties import MONTECARLO_ITERATIONS
 from ciscox83.montecarlo.core.iteration import Iteration
-from ciscox83.montecarlo.core.montecarlo import Montecarlo
+from ciscox83.montecarlo.core.epic_simulation import EpicSimulator
 
 
 class MontecarloUnitTest(unittest.TestCase):
 
-    def test_that_can_compute_real_cycle_times(self):
+    def test_that_can_compute_past_cycle_times(self):
         iterations = [
             Iteration(7, "2018/12/08 00:00", "2018/12/15 23:59"),
             Iteration(8, "2018/12/15 00:00", "2018/12/21 23:59"),
             Iteration(9, "2018/12/21 00:00", "2018/12/28 23:59")
         ]
 
-        montecarlo = Montecarlo(iterations)
-        cycle_times = montecarlo.real_cycle_times
+        epic_simulator = EpicSimulator(iterations)
+        cycle_times = epic_simulator.past_cycle_times
         self.assertEqual(cycle_times,
                          [Decimal(0.71).__round__(2),
                           Decimal(0.62).__round__(2),
                           Decimal(0.56).__round__(2)])
 
-    def test_that_can_compute_average_iterations_to_complete_pending_work_items(self):
+    def test_that_can_compute_simulated_iterations(self):
         items_completed_in_iteration = 7
         pending_items = 21
         past_iterations = [
@@ -31,12 +31,12 @@ class MontecarloUnitTest(unittest.TestCase):
             Iteration(items_completed_in_iteration, "2018/12/21 00:00", "2018/12/28 23:59")
         ]
 
-        montecarlo = Montecarlo(past_iterations)
-        average_iterations = montecarlo.compute_average_iterations_forecast(pending_items)
-        num_of_average_iterations = len(average_iterations)
-        self.assertEqual(num_of_average_iterations, MONTECARLO_ITERATIONS)
-        for i in range(0, num_of_average_iterations, 1):
-            self.assertEqual(average_iterations[i], pending_items / items_completed_in_iteration)
+        epic_simulator = EpicSimulator(past_iterations)
+        simulated_iterations_length = epic_simulator.simulate(pending_items)
+        num_of_simulated_iterations = len(simulated_iterations_length)
+        self.assertEqual(num_of_simulated_iterations, MONTECARLO_ITERATIONS)
+        for i in range(0, num_of_simulated_iterations, 1):
+            self.assertEqual(simulated_iterations_length[i], pending_items / items_completed_in_iteration)
 
 
 
