815a9d76f9cc10e298f7efa8070cb58794119a63
Christian Sisti <christian.sisti@ocado.com>
2019-05-08 09:05:28 +0200

    Implement interpolation to get forecasted approximated duration

--- ciscox83/tests/unit/test_interpolator.py
+++ ciscox83/tests/unit/test_interpolator.py
@@ -1,28 +1,20 @@
 import unittest
 
-from ciscox83.when_it_will_be_done.core.distribution_entry import DistributionEntry
 from ciscox83.when_it_will_be_done.core.interpolator import Interpolator
 
 
 class InterpolatorUnitTest(unittest.TestCase):
 
     def test_that_can_infer_forecasted_iteration_length(self):
-        entry1 = DistributionEntry(4, 14)
-        entry2 = DistributionEntry(5, 33)
-        entry3 = DistributionEntry(6, 58)
-        entry4 = DistributionEntry(7, 77)
-        entry5 = DistributionEntry(8, 90)
-        entry6 = DistributionEntry(9, 96)
-        entry7 = DistributionEntry(10, 100)
+        cumulative_percentages = [14, 33, 58, 77, 90, 96, 100]
+        iteration_lenghts = [4, 5, 6, 7, 8, 9, 10]
 
-        iteration_length_to_cumulative_percentages = [entry1, entry2, entry3, entry4, entry5, entry6, entry7]
-
-        desired_cumulative_percentage = 80
+        target_cumulative_percentage = 80
         expected_interpolated_iteration_length = 7.2
 
         interpolator = Interpolator()
-        interpolated_iteration_length = interpolator.quadratic_interpolation(
-            iteration_length_to_cumulative_percentages, desired_cumulative_percentage)
+        interpolated_iteration_length = interpolator.approximate_forecasted_duration(
+            cumulative_percentages, iteration_lenghts, target_cumulative_percentage)
 
         self.assertEqual(expected_interpolated_iteration_length, interpolated_iteration_length)
 
--- ciscox83/when_it_will_be_done/core/interpolator.py
+++ ciscox83/when_it_will_be_done/core/interpolator.py
@@ -1,6 +1,11 @@
+from scipy.interpolate import interp1d
 
 class Interpolator:
     def __init__(self):
         pass
 
-    def quadratic_interpolation(self, iteration_length_to_cumulative_percentages, cumulative_percentage):
+    def approximate_forecasted_duration(self, cumulative_percentages, iteration_lenghts, target_cumulative_percentage):
+        x = cumulative_percentages
+        y = iteration_lenghts
+        f = interp1d(x, y, kind='cubic')
+        return round(f(target_cumulative_percentage).item(0), 1)
