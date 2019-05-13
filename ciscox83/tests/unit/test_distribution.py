56aee85ac4505e9fb9746adc9938624ce1274aa2
Christian Sisti <christian.sisti@ocado.com>
2019-05-13 10:07:45 +0200

    Refactoring

--- ciscox83/tests/unit/test_distribution.py
+++ ciscox83/tests/unit/test_distribution.py
@@ -5,16 +5,16 @@ from ciscox83.when_it_will_be_done.core.distribution import Distribution
 class DistributionUnitTest(unittest.TestCase):
 
     def test_that_can_get_unique_iteration_lengths(self):
-        distribution = Distribution()
         simulated_iteration_lengths = [4, 2, 3, 3, 3, 1, 1]
+        distribution = Distribution(simulated_iteration_lengths)
         expected_unique_simulated_iteration_lengths = [1, 2, 3, 4]
-        unique_simulated_iteration_lengths = distribution.get_unique_simulated_iteration_lenghts(simulated_iteration_lengths)
+        unique_simulated_iteration_lengths = distribution.get_unique_simulated_iteration_lengths()
         for i in (0, 3):
             self.assertEqual(expected_unique_simulated_iteration_lengths[i], unique_simulated_iteration_lengths[i])
 
     def test_that_can_get_cumulative_percentages(self):
         simulated_iteration_lengths = [4, 2, 3, 3, 3, 1, 1]
-        distribution = Distribution()
+        distribution = Distribution(simulated_iteration_lengths)
         expected_cumulative_percentages = [29, 43, 86, 100]
 
         cumulative_percentages = distribution.get_cumulative_percentages(simulated_iteration_lengths)
--- ciscox83/when_it_will_be_done/core/distribution.py
+++ ciscox83/when_it_will_be_done/core/distribution.py
@@ -1,16 +1,15 @@
 class Distribution:
-    def __init__(self):
+    def __init__(self, simulated_iteration_lengths):
+        self.unique_simulated_iteration_lengths = sorted(list(dict.fromkeys(simulated_iteration_lengths)))
         pass
 
-    @staticmethod
-    def get_unique_simulated_iteration_lenghts(simulated_iteration_lengths):
-        return sorted(list(dict.fromkeys(simulated_iteration_lengths)))
+    def get_unique_simulated_iteration_lengths(self):
+        return self.unique_simulated_iteration_lengths
 
     def get_cumulative_percentages(self, simulated_iteration_lengths):
-        unique_simulated_iteration_lengths = self.get_unique_simulated_iteration_lenghts(simulated_iteration_lengths)
         cumulative_percentages = []
         cumulative_percentage = 0
-        for iteration_length in unique_simulated_iteration_lengths:
+        for iteration_length in self.unique_simulated_iteration_lengths:
             count = simulated_iteration_lengths.count(iteration_length)
             percentage = round(count * 100 / len(simulated_iteration_lengths), 0)
             cumulative_percentage = cumulative_percentage + percentage
