aa6f0c279b7a856ca3ecfecb4fd76ada01908c04
Christian Sisti <christian.sisti@ocado.com>
2019-05-13 10:03:00 +0200

    Tidy up

--- ciscox83/tests/unit/test_distribution.py
+++ ciscox83/tests/unit/test_distribution.py
@@ -4,18 +4,6 @@ from ciscox83.when_it_will_be_done.core.distribution import Distribution
 
 class DistributionUnitTest(unittest.TestCase):
 
-    def test_that_distribution_order_is_correct(self):
-        simulated_iteration_lengths = [4, 2, 3, 3, 3, 1, 1]
-        distribution = Distribution()
-        iteration_length_to_cumulative_percentages = distribution.iteration_length_to_cumulative_percentages(simulated_iteration_lengths)
-
-        expected_durations = [1, 2, 3, 4]
-        expected_cumulative_percentages = [29, 43, 86, 100]
-
-        for i in (0, 3):
-            self.assertEqual(iteration_length_to_cumulative_percentages[i].get_duration(), expected_durations[i])
-            self.assertEqual(iteration_length_to_cumulative_percentages[i].get_cumulative_percentage(), expected_cumulative_percentages[i])
-
     def test_that_can_get_unique_iteration_lengths(self):
         distribution = Distribution()
         simulated_iteration_lengths = [4, 2, 3, 3, 3, 1, 1]
--- ciscox83/when_it_will_be_done/core/distribution.py
+++ ciscox83/when_it_will_be_done/core/distribution.py
@@ -1,24 +1,6 @@
-from ciscox83.when_it_will_be_done.core.distribution_entry import DistributionEntry
-
-
-
 class Distribution:
     def __init__(self):
         pass
-
-    @staticmethod
-    def iteration_length_to_cumulative_percentages(simulated_iteration_lengths):
-        unique_simulated_iteration_lengths = set(simulated_iteration_lengths)
-
-        iteration_length_to_cumulative_percentages = []
-        cumulative_percentage = 0
-        for iteration_length in unique_simulated_iteration_lengths:
-            count = simulated_iteration_lengths.count(iteration_length)
-            percentage = round(count * 100 / len(simulated_iteration_lengths), 0)
-            cumulative_percentage = cumulative_percentage + percentage
-            iteration_length_to_cumulative_percentages.append(DistributionEntry(iteration_length, cumulative_percentage))
-
-        return iteration_length_to_cumulative_percentages
 
     @staticmethod
     def get_unique_simulated_iteration_lenghts(simulated_iteration_lengths):
