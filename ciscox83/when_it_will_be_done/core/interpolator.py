05f317314dfccf878cdb133977e0d4eee0d3d693
Christian Sisti <christian.sisti@ocado.com>
2019-05-13 10:09:02 +0200

    Refactoring

--- ciscox83/when_it_will_be_done/core/interpolator.py
+++ ciscox83/when_it_will_be_done/core/interpolator.py
@@ -4,8 +4,9 @@ class Interpolator:
     def __init__(self):
         pass
 
-    def approximate_forecasted_duration(self, cumulative_percentages, iteration_lenghts, target_cumulative_percentage):
+    @staticmethod
+    def approximate_forecasted_duration(cumulative_percentages, iteration_lengths, target_cumulative_percentage):
         x = cumulative_percentages
-        y = iteration_lenghts
+        y = iteration_lengths
         f = interp1d(x, y, kind='cubic')
         return round(f(target_cumulative_percentage).item(0), 1)
