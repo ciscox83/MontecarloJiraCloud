c00a8b6a5e4e574794853ce07038d17dfe78168c
Christian Sisti <christian.sisti@ocado.com>
2019-05-13 10:01:08 +0200

    Get cumulative percentages

--- ciscox83/tests/unit/test_distribution.py
+++ ciscox83/tests/unit/test_distribution.py
@@ -24,11 +24,15 @@ class DistributionUnitTest(unittest.TestCase):
         for i in (0, 3):
             self.assertEqual(expected_unique_simulated_iteration_lengths[i], unique_simulated_iteration_lengths[i])
 
-    # def test_that_can_get_cumulative_percentages(self):
-    #     simulated_iteration_lengths = [4, 2, 3, 3, 3, 1, 1]
-    #     distribution = Distribution()
-    #     expected_cumulative_percentages = [29, 43, 86, 100]
+    def test_that_can_get_cumulative_percentages(self):
+        simulated_iteration_lengths = [4, 2, 3, 3, 3, 1, 1]
+        distribution = Distribution()
+        expected_cumulative_percentages = [29, 43, 86, 100]
 
+        cumulative_percentages = distribution.get_cumulative_percentages(simulated_iteration_lengths)
+
+        for i in (0, 3):
+            self.assertEqual(expected_cumulative_percentages[i], cumulative_percentages[i])
 
 
 if __name__ == '__main__':
--- ciscox83/when_it_will_be_done/core/distribution.py
+++ ciscox83/when_it_will_be_done/core/distribution.py
@@ -23,3 +23,14 @@ class Distribution:
     @staticmethod
     def get_unique_simulated_iteration_lenghts(simulated_iteration_lengths):
         return sorted(list(dict.fromkeys(simulated_iteration_lengths)))
+
+    def get_cumulative_percentages(self, simulated_iteration_lengths):
+        unique_simulated_iteration_lengths = self.get_unique_simulated_iteration_lenghts(simulated_iteration_lengths)
+        cumulative_percentages = []
+        cumulative_percentage = 0
+        for iteration_length in unique_simulated_iteration_lengths:
+            count = simulated_iteration_lengths.count(iteration_length)
+            percentage = round(count * 100 / len(simulated_iteration_lengths), 0)
+            cumulative_percentage = cumulative_percentage + percentage
+            cumulative_percentages.append(cumulative_percentage)
+        return cumulative_percentages
